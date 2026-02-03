726b253f46d1319c1c96d0538c9de1111833afac
SurajPawar15-smart <supersuraj143@gmail.com>
2026-02-03 21:56:22 +0530

    initialised project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Layout } from './pages/layout/layout';
+import { Login } from './pages/login/login';
+import { ClientData } from './pages/client/client-data/client-data';
+import { EmployeeList } from './pages/employee/employee-list/employee-list';
+import { EmployeeForm } from './pages/employee/employee-form/employee-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'client-data', component: ClientData },
+      { path: 'employee-list', component: EmployeeList },
+      { path: 'employee-form', component: EmployeeForm },
+    ],
+  },
+];
