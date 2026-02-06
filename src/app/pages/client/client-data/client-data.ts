d0e069bbaf62cb4a97a1ee2e89635e639f643261
SurajPawar15-smart <supersuraj143@gmail.com>
2026-02-06 10:16:04 +0530

    client data Form added

--- src/app/pages/client/client-data/client-data.ts
+++ src/app/pages/client/client-data/client-data.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-client-data',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './client-data.html',
   styleUrl: './client-data.css',
 })
 export class ClientData {
+  clientForm: FormGroup = new FormGroup({});
+  formBuilder = inject(FormBuilder);
 
+  initializeForm() {
+    this.clientForm = new FormGroup({
+      clientId: new FormControl(0),
+      contactPersonName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      companyName: new FormControl(''),
+      address: new FormControl(''),
+      city: new FormControl(''),
+      pincode: new FormControl(''),
+      state: new FormControl(''),
+      EmployeeStrength: new FormControl(''),
+      gstNo: new FormControl(''),
+      contactNo: new FormControl(''),
+      regNo: new FormControl(''),
+    });
+    this.clientForm = this.formBuilder.group({
+      clientId: new FormControl(0),
+      contactPersonName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    });
+  }
+
+  opneModal() {
+    const modal = document.getElementById('myModal');
+    if (modal != null) {
+      modal.style.display = 'block';
+    }
+    //document.getElementById('myModal').style.display = 'block';
+    // debugger;
+    // if(this.modalInstance) {
+    //   this.modalInstance.nativeElement.style.display = 'block'
+    // }
+
+    // const alertComp =  this.alertCompoInstance.alertTitle;
+  }
 }
